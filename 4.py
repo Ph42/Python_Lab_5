2c595aa7e513f7035d96942b394c87dac4b6710b
Ph42 <metalcobalt@gmail.com>
2017-06-05 14:58:38 +0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,21 @@
+#Lab 5
+#Дан целочисленный квадратный массив 7x7. Найти строку с наибольшей суммой элементов.
+import random
+
+#listOfNumbers = [random.randint(-50, 50) for i in range(15)]
+listOfNumbers = [int(i) for i in input('Введите через пробел элементы последовательности:\n\t').split()]
+# Образуют ли они арифметическую проггрессию. Если да то вывести разность прогрессии
+isArithmeticProgression = False
+if len(listOfNumbers) > 1:
+	d = listOfNumbers[1] - listOfNumbers[0]	
+	i = 2
+	isArithmeticProgression = True
+	while (i < len(listOfNumbers)) and isArithmeticProgression:
+		if listOfNumbers[i] - listOfNumbers[i-1] != d:
+			isArithmeticProgression = False
+		i += 1
+if isArithmeticProgression:
+	print('Последовательность является арифметической прогрессией. Разность прогрессии d = {}'.format(d))
+else:
+	print('Числа последовательности не образуют арифметической прогрессии')
+input()
