50c88f4ba6666bced56d7a4ec18631851f1cb638
Ph42 <metalcobalt@gmail.com>
2017-06-05 14:30:12 +0500

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,27 +1,19 @@
 import random
 
 #listOfNumbers = [random.randint(-50, 50) for i in range(15)]
-listOfNumbers = [i for i in input('Введите через пробел элементы последовательности').split()]
+listOfNumbers = [int(i) for i in input('Введите через пробел элементы последовательности:\n\t').split()]
 # Образуют ли они арифметическую проггрессию. Если да то вывести разность прогрессии
-
-'''
-print('posOfFirstNegative = ', posOfFirstNegative)
-print('posOfFirstZero = ', posOfFirstZero)
-
-if (posOfFirstNegative < 0) or (posOfFirstZero < 0):
-	print('В списке отсутствует одно или оба из искомых чисел границы интервала: отрицательное и равное нулю')
+isArithmeticProgression = False
+if len(listOfNumbers) > 1:
+	d = listOfNumbers[1] - listOfNumbers[0]	
+	i = 2
+	isArithmeticProgression = True
+	while (i < len(listOfNumbers)) and isArithmeticProgression:
+		if listOfNumbers[i] - listOfNumbers[i-1] != d:
+			isArithmeticProgression = False
+		i += 1
+if isArithmeticProgression:
+	print('Последовательность является арифметической прогрессией. Разность прогрессии d = {}'.format(d))
 else:
-	sum = 0
-	i = 0
-	a, b = posOfFirstNegative, posOfFirstZero # Границы интервала
-	if a > b:
-		a, b = b, a
-	slice1 = []
-	for i in range(a+1,b):
-		sum += listOfNumbers[i]
-		slice1.append(listOfNumbers[i])
-		i +=1
-	print('Искомые элементы = listOfNumbers[{:4.2f}:{}] = {}'.format(a+1,b,slice1))
-	print('Сумма всех чисел между первым отрицательным и первым нулевым элементами\n\t= {}'.format(sum))
+	print('Числа последовательности не образуют арифметической прогрессии')
 input()
-'''
